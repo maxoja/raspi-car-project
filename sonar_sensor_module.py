3cc99fe0db1c2f9318b2e733a5e045e0a870391f
maxoja <tawan.tpptnk@gmail.com>
2017-12-13 01:32:39 +0700

    change trigger/echo pin based on GPIO BOARD numbering mode. update main test

--- sonar_sensor_module.py
+++ sonar_sensor_module.py
@@ -6,8 +6,11 @@ from hcsr04sensor import sensor
 #25 C = 77 F
 #30 C = 86 F
 
-trigger_pin     = 23
-echo_pin        = 24
+trigger_pin     = 16
+echo_pin        = 18
+
+sample_size = 1
+sample_interval = 0.05
 
 def check_distance() :
 
@@ -22,35 +25,20 @@ def check_distance() :
         )
 
         raw_measurement = value.raw_distance(
-            sample_size = 3,
-            sample_wait = 0.04
+            sample_size = sample_size,
+            sample_wait = sample_interval
         )
 
         # Calculate the distance in inches
         inch_distance = value.distance_imperial(raw_measurement)
         
-    except :
-        
+    except Exception as e:
+##        print(e)
         inch_distance = None
 
     return inch_distance
 
-#### unit test #####
+#### test #########
 if __name__ == '__main__' :
-    import threading
-    
-    class SonarSensorThread(threading.Thread):
-        def __init__(self):
-            threading.Thread.__init__(self)
-            
-        def run(self):
-            while True:
-                print(check_distance())
-
-    def test():
-        distance = SonarSensorThread()
-        distance.start()
-        distance.join()
-        
-    if __name__ == "__main__":
-        test()
+    while True :
+        print(check_distance())
