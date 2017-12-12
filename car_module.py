837cd733033812261d1ca1f658e6362da41aeb9c
maxoja <tawan.tpptnk@gmail.com>
2017-12-13 01:33:57 +0700

    created car module and interface(curse shell) module

---
+++ car_module.py
@@ -0,0 +1,89 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+def deinit() :
+    __cleanup_gpio()
+    
+def stop() :
+    __set_duty_cycle_for_pins(0, 0, 0, 0)
+
+def turn_left(duty_cycle):
+    __set_duty_cycle_for_pins(0, duty_cycle, duty_cycle, 0)
+
+def turn_right(duty_cycle):
+    __set_duty_cycle_for_pins(duty_cycle, 0, 0, duty_cycle)
+    
+
+def move_forward(duty_cycle):
+    __set_duty_cycle_for_pins(duty_cycle, 0, duty_cycle, 0)
+
+def move_backward(duty_cycle):
+    __set_duty_cycle_for_pins(0, duty_cycle, 0, duty_cycle)
+
+def __set_duty_cycle(pin_id, dc) :
+    pwm_pins[pin_id].ChangeDutyCycle(dc)
+
+def __set_duty_cycle_for_pins(a,b,c,d) :
+    __set_duty_cycle(pin_left_a,a)
+    __set_duty_cycle(pin_left_b,b)
+    __set_duty_cycle(pin_right_a,c)
+    __set_duty_cycle(pin_right_b,d)
+    
+def __cleanup_gpio() :
+    GPIO.cleanup()
+
+def __init_pwm_pin(pin_id) :
+    pwm_pins[pin_id] = GPIO.PWM(pin_id, pwm_frequency)
+    pwm_pins[pin_id].start(0)
+
+def __init_output_pin(pin_id) :
+    GPIO.setup(pin_id, GPIO.OUT)
+    
+def __init_gpio_pins() :
+    GPIO.setmode(GPIO.BOARD)
+
+    __init_output_pin(pin_left_a)
+    __init_output_pin(pin_left_b)
+    __init_output_pin(pin_right_a)
+    __init_output_pin(pin_right_b)
+
+    __init_pwm_pin(pin_left_a)
+    __init_pwm_pin(pin_left_b)
+    __init_pwm_pin(pin_right_a)
+    __init_pwm_pin(pin_right_b)
+
+class ControlValues :
+    def  __init__(self) :
+        self.direction = 'none'
+        self.duty_cycle = 50
+        
+    def set_direction(self, direction) :
+        self.direction = direction
+
+    def set_duty_cycle(self, duty_cycle) :
+        self.duty_cycle = duty_cycle
+        self.duty_cycle = 100 if self.duty_cycle > 100 else self.duty_cycle
+        self.duty_cycle = 0 if self.duty_cycle < 0 else self.duty_cycle
+
+    def increase_duty_cycle(self) :
+        self.set_duty_cycle(self.duty_cycle+1)
+
+    def decrease_duty_cycle(self) :
+        self.set_duty_cycle(self.duty_cycle-1)
+
+    def get_duty_cycle(self) :
+        return self.duty_cycle
+
+    def get_direction(self) :
+        return self.direction
+
+## initialize ####
+pwm_frequency = 50
+pwm_pins = dict()
+
+pin_left_a = 13
+pin_left_b = 15
+pin_right_a = 7
+pin_right_b = 11
+
+__init_gpio_pins()
---
+++ interface_module.py
@@ -0,0 +1,42 @@
+import curses
+
+def deinit() :
+    __screen.clear()
+    curses.endwin()
+
+def get_key_pressed() :
+    key = __screen.getch()
+    __screen.addstr(5,5, 'keycode:' + str(key) + ' - key:' + chr(key))
+##    __screen.addch(10,0,key) #debug pressed key code
+    __screen.refresh()
+    return __key_map[key] if key in __key_map else key
+
+def __init_screen() :
+    screen = curses.initscr()
+    curses.cbreak()
+    screen.keypad(1) #set 1 to let curses accept escape keys
+
+    # (y-position, x-position, string)
+    screen.addstr(0, 10, "hit 'q' to quit")
+    screen.addstr(1, 5, "press arrow keys to controll")
+
+    # update display to show whatever drawn
+    screen.refresh()
+
+    return screen
+
+## initialize #####
+__screen = None
+__key_map = {
+    curses.KEY_UP : 'up',
+    curses.KEY_DOWN : 'down',
+    curses.KEY_LEFT : 'left',
+    curses.KEY_RIGHT : 'right',
+    32 : 'space',
+    49 : '1',
+    50 : '2',
+    ord('q') : 'q'
+}
+
+__screen = __init_screen()
+curses.noecho()
