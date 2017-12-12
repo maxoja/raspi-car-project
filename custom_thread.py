84608be32eb697f48d411a51551b42106df3943f
maxoja <tawan.tpptnk@gmail.com>
2017-12-13 01:31:05 +0700

    created custom thread class ( WhileTrueThread )

---
+++ custom_thread.py
@@ -0,0 +1,33 @@
+import threading
+from time import sleep
+
+class WhileTrueThread(threading.Thread) :
+    
+    def __init__(self, interval) :
+        threading.Thread.__init__(self)
+        self.__interval = interval
+        self.__stop = False
+
+    def stop(self) :
+        self.__stop = True
+        
+    def _prepare(self) :
+        pass
+
+    def _loop(self) :
+        pass
+
+    def _end(self) :
+        pass
+
+    def run(self) :
+        
+        self._prepare()
+        
+        while not self.__stop :
+            self._loop()
+            sleep(self.__interval)
+
+        self._end()
+            
+            
